10c741357afd62b277c530bdca6852f41e8e202d
Cruz Bermudez <diego.cruzbermudez@bsci.com>
2026-05-09 12:06:39 -0600

    Clase 02

---
+++ Clase 02/main.py
@@ -0,0 +1,19 @@
+print ("hola mundo")
+
+nombre = "Diego"
+edad:int = int (input ("cual es tu edad "))
+anno_de_nacimiento:int = 2026 - edad 
+print (anno_de_nacimiento)
+mayor_edad:bool = edad >=18
+print (mayor_edad)
+
+soy_yo = nombre == "Diego" and edad ==23
+quizas_soy_yo = nombre == "Diego" or edad == 23
+no_soy_yo = nombre == "Diego" or edad == 23
+
+print (soy_yo)
+print (no_soy_yo)
+print (quizas_soy_yo)
+
+x = 10
+x == 5
