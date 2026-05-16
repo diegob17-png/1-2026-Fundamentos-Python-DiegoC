d4f4e6e676fd4b96c49810f6ac484224f61e9695
diegob17-png <diegocruzb17@gmail.com>
2026-05-16 11:21:23 -0600

    Comentar la clase para mejorar entendimiento

--- Clase 02/main.py
+++ Clase 02/main.py
@@ -1,12 +1,21 @@
+#Hola mundo es para abrir cualquier code, no tiene funcion
 print ("hola mundo")
 
+#variables del code (inicializar variables)
 nombre = "Diego"
 edad:int = int (input ("cual es tu edad "))
+#calcular año de nacimiento 
 anno_de_nacimiento:int = 2026 - edad 
+# no mas variables aca 
+
+#Imprimir nombre edad y año
 print (anno_de_nacimiento)
+
+#se calcula si es menor de edad 
 mayor_edad:bool = edad >=18
 print (mayor_edad)
 
+#inicializacion de variables boleanas (si o no, true, false)
 soy_yo = nombre == "Diego" and edad ==23
 quizas_soy_yo = nombre == "Diego" or edad == 23
 no_soy_yo = nombre == "Diego" or edad == 23
@@ -17,3 +26,4 @@ print (quizas_soy_yo)
 
 x = 10
 x == 5
+
